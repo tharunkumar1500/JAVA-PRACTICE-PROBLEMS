2a66d2781d5684ebe1ec6fb5b2f168dde901d24e
tharunkumar1500 <tharunkumar13072@gmail.com>
2026-01-09 12:41:39 +0530

    day 2 if else problem 1

--- Practise/src/day2/practice/if_else_statement.java
+++ Practise/src/day2/practice/if_else_statement.java
@@ -4,36 +4,55 @@ import java.util.Scanner;
 
 public class if_else_statement {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+//        Scanner sc = new Scanner(System.in);
+//
+//
+//        System.out.print("Enter a single digit number 0 to 9: ");
+//
+//        int num = sc.nextInt();
+//
+//        if (num == 0)
+//            System.out.println("Zero");
+//        else if (num == 1)
+//            System.out.println("One");
+//        else if (num == 2)
+//            System.out.println("Two");
+//        else if (num == 3)
+//            System.out.println("Three");
+//        else if (num == 4)
+//            System.out.println("Four");
+//        else if (num == 5)
+//            System.out.println("Five");
+//        else if (num == 6)
+//            System.out.println("Six");
+//        else if (num == 7)
+//            System.out.println("Seven");
+//        else if (num == 8)
+//            System.out.println("Eight");
+//        else if (num == 9)
+//            System.out.println("Nine");
+//        else
+//            System.out.println("Invalid input! Please enter a single digit.");
+//
+//        sc.close();
 
+//2 example
+        Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter a single digit number 0 to 9: ");
+        System.out.print("Enter a number (1, 10, 100, 1000): ");
+        int number  = input.nextInt();
 
-        int num = sc.nextInt();
-
-        if (num == 0)
-            System.out.println("Zero");
-        else if (num == 1)
-            System.out.println("One");
-        else if (num == 2)
-            System.out.println("Two");
-        else if (num == 3)
-            System.out.println("Three");
-        else if (num == 4)
-            System.out.println("Four");
-        else if (num == 5)
-            System.out.println("Five");
-        else if (num == 6)
-            System.out.println("Six");
-        else if (num == 7)
-            System.out.println("Seven");
-        else if (num == 8)
-            System.out.println("Eight");
-        else if (num == 9)
-            System.out.println("Nine");
+        if (number == 1)
+            System.out.println("Unit");
+        else if (number == 10)
+            System.out.println("Ten");
+        else if (number == 100)
+            System.out.println("Hundred");
+        else if (number == 1000)
+            System.out.println("Thousand");
         else
-            System.out.println("Invalid input! Please enter a single digit.");
+            System.out.println("Invalid number! Please enter 1, 10, 100, or 1000.");
 
-        sc.close();
+        input.close();
     }
 }
